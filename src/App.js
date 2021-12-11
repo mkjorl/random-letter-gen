8ba9bb25a5d578b6638cb0a4bfe32d57965b65dd
Mark Kjorlien <43691578+mkjorl@users.noreply.github.com>
2021-12-11 01:15:26 -0700

    niceify

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import { CSSTransition, SwitchTransition, Transition } from 'react-transition-group';
 
 function App() {
-  
+
 
   const [selectedLetter, setSelectedLetter] = useState(0);
+  let timer = 2000;
   const letters = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"
+    "a", "b", "c", "d",
+    "e", "f", "g", "h",
+    "i", "j", "k", "l",
+    "m", "n", "o", "p",
+    "q", "r", "s", "t",
+    "u", "v", "w", "x",
+    "y", "z"
   ]
 
   function randomInteger(min, max) {
-    setSelectedLetter(Math.floor(Math.random() * (max - min + 1)) + min);
+
+  }
+
+
+  const nextFrame = () => {
+
+    if (timer > 0) {
+      setSelectedLetter(Math.floor(Math.random() * (25 - 0 + 1)) + 0);
+      timer = timer - 100;
+      setTimeout(nextFrame, 100);
+    }
   }
 
   return (
     <div className="App">
-        <div className="box" onClick={() => {randomInteger(0, 27)}}>
-          <h1>{letters[selectedLetter]}</h1>
-        </div>
+      <div className="box" onClick={() => { nextFrame() }}>
+        <SwitchTransition>
+          <CSSTransition
+            key={selectedLetter}
+            addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
+            classNames='fade'>
+            <span className="letter">{letters[selectedLetter]}</span>
+          </CSSTransition>
+        </SwitchTransition>
+      </div>
     </div>
   );
 }
